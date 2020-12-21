c47bb3b9e80260d1d61c296f47e4505691a74428
SAI RAM KRISHNA DURGI <sairamkrishna1412@gmail.com>
2020-12-21 10:51:16 +0530

    add

---
+++ budgety/starter/src/js/views/base.js
@@ -0,0 +1,14 @@
+export const elements = {
+    transactionType : document.querySelector('.add__type'),
+    description : document.querySelector('.add__description'),
+    value : document.querySelector('.add__value'),
+    submitButton : document.querySelector('.add__btn'),
+    incomeList : document.querySelector('.income__list'),
+    expenseList : document.querySelector('.expenses__list'),
+    monthText : document.querySelector('.budget__title--month'),
+    availableBudget : document.querySelector('.budget__value'),
+    totalIncome : document.querySelector('.budget__income--value'),
+    totalExpense : document.querySelector('.budget__expenses--value'),
+    expTotalPercentage : document.querySelector('.budget__expenses--percentage'),
+    container : document.querySelector('.container') 
+}
